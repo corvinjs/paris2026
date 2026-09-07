d0f8c135b6a489c98ef7c9d466a784a05505b617
corvinjs <174349145+corvinjs@users.noreply.github.com>
2026-09-07 19:08:21 +0200

    you came for the drama, found placeholder commit msg

--- publish.sh
+++ publish.sh
@@ -6,7 +6,7 @@ git add . >/dev/null 2>&1
 
 # Only commit if there are actual changes staged
 if ! git diff --cached --quiet; then
-    git commit -m "Auto-Update" -q >/dev/null 2>&1
+    git commit -m "still alive" -q >/dev/null 2>&1
     echo Created new commit
 fi
 
